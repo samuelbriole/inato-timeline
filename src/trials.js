29e4fdb14fea880aa6947dadc79dc0f3cca05f8f
Samuel Briole <samuel.briole@gmail.com>
2018-02-25 17:20:45 +0000

    add renderClinicalTrials function returning null

---
+++ src/trials.js
@@ -0,0 +1,6 @@
+export default [
+  { start: 5, end: 50, title: "Study of Bendamustine" },
+  { start: 55, end: 85, title: "ASCT With Nivolumab" },
+  { start: 70, end: 100, title: "Study of Stockolm" },
+  { start: 90, end: 115, title: "Bortezomib" }
+];
